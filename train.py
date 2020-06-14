6fb7dcd2f0ee3c2bc12d936e0389bdc72d5f81c5
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-15 01:28:37 +0900

    update

--- train.py
+++ train.py
@@ -445,6 +445,9 @@ def evaluate(model, loss_fn, dataloader, metrics, params, args, summ_maker=None)
         # summaries already written.
         tbx_index = 0
         for i, sample in enumerate(dataloader):
+            image_write(ref_img_batch, 'ref.jpg')
+            image_write(search_batch, 'srch.jpg')
+            print('labels_batch = {}'.format(labels_batch))
             ref_img_batch = sample['ref_frame'].to(device)
             search_batch = sample['srch_frame'].to(device)
             labels_batch = sample['label'].to(device)
